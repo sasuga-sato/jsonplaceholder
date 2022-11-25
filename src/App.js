a701bc21f98cf1493e4a06b1b72522089d7c2908
sasuga-sato <117708142+sasuga-sato@users.noreply.github.com>
2022-11-25 13:47:04 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const onClickUser1 = () => {
+    //URLの書き方が二つあるhttps://jsonplaceholder.typicode.com/users/1
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
+}
